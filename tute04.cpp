a0271bbd71a5c5a331f8e75c00259c17944dd51f
Hiran <99481438+hirankaushalya@users.noreply.github.com>
2022-02-15 04:45:17 +0000

    I modified given program 1,2,3 as a comment.

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,17 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  int i,temp=1;
+
+  for(i=1;i<=no;i++){
+    temp=temp * i;
+    }
+  return temp;
+}
+
+long nCr(int n, int r)
+{
+  return (Factorial(n)/(Factorial(r)*Factorial(n-r)));
+}
